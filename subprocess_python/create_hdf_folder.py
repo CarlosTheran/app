436fd0669ee2ce90492c3b591c2a4f6ed1a5336b
CarlosTheran <carlos.theran@upr.edu>
2020-11-24 15:22:43 +0000

    working

--- subprocess_python/create_hdf_folder.py
+++ subprocess_python/create_hdf_folder.py
@@ -2,9 +2,9 @@
 
 import subprocess
 from subprocess import Popen
+import sys
 
-
-def hadoop_new_folder(path)
+def hadoop_new_folder(path):
     #the parameter path must start with the simbol /, e.x /path
     full_command = 'hadoop dfs -mkdir '+ path
     p = Popen(full_command , shell=True)
@@ -13,16 +13,24 @@ def hadoop_new_folder(path)
     output = p.stdout
     return output
 
-def hadoop_upload_data(path,name_file)
+def hadoop_delete_folder(path):
+    #the parameter path must start with the simbol /, e.x /path
+    full_command = 'hadoop dfs -rm -r '+ path
+    p = Popen(full_command , shell=True)
+    p.wait()
+    p = Popen('hadoop dfs -ls /' , shell=True)
+    output = p.stdout
+    return output
+
+def hadoop_upload_data(path,name_file):
     full_command = 'hadoop dfs -put '+ path + '/' + name_file
     p = Popen(full_command , shell=True)
     p.wait()
 
-def hadoop_download_data(path,name_file)
+def hadoop_download_data(path,name_file):
     full_command = 'hadoop dfs -get '+ path + '/' + name_file 
     p = Popen(full_command , shell=True)
     p.wait()
- 
 
 #p = Popen('hadoop dfs -mkdir /test_delete', shell=True)
 #p.wait()
@@ -32,8 +40,10 @@ def hadoop_download_data(path,name_file)
 
 
 def main():
-    print("python main function")
-
+    print("python main function folder name", sys.argv[1])
+    #str = '/test_delete'
+    str = sys.argv[1]
+    hadoop_new_folder(str)
 
 if __name__ == '__main__':
    main()
